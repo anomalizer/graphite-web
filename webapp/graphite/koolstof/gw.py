984c02c9acc0a214aa712550a56f330442f2cd5e
Arvind Jayaprakash <work@anomalizer.net>
2013-07-06 20:24:27 +0530

    to recurse is divine

--- webapp/graphite/koolstof/gw.py
+++ webapp/graphite/koolstof/gw.py
@@ -1,3 +1,4 @@
+import fnmatch
 from graphite.node import BranchNode, LeafNode
 from graphite.logger import log
 
@@ -7,4 +8,19 @@ class KoolstofFinder(object):
         pass
 
     def find_nodes(self, query):
-        return None
+        #TODO: not ignore time components
+
+        return self._find_nodes(query.pattern.split('.'), 0, '')
+
+    def _find_nodes(self, parts, current_level, parent_path):
+
+        if len(parts) == current_level:
+            yield parent_path  # TODO: verify it exists
+        else:
+            component = parts[current_level]
+            if '*' in component:
+                pass  # TODO :real impl
+            else:
+                new_path = '%s.%s' % (parent_path, component) if parent_path else component
+                for x in self._find_nodes(parts, current_level + 1, new_path):
+                    yield x
